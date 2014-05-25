1692a1a134c0cfeec1049fbe50ab57cf43293570
HaikoEitzen <haiko.eitzen@gmail.com>
2014-05-25 02:32:53 -0400

    class created, getters and setters and stuff

---
+++ CashTrack/src/CashTrackClasses/Account.java
@@ -0,0 +1,119 @@
+/**
+ *  CashTrack
+ *  Copyright (C) 2014  Haiko René Eitzen Bartel
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package CashTrackClasses;
+
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+
+/**
+ * I think this class is practically self-explanatory.
+ * More documentation will follow in the future.
+ * 
+ * The light shines in the darkness, but the darkness has not understood it.
+ * John 1:5
+ * 
+ * @author Haiko
+ */
+public class Account {
+    
+    private final Integer id;
+    private String name;
+    private Double balance;
+    private ArrayList<Transaction> movement;
+    private Boolean autoPull;
+    private Account autoPullFrom;
+    private Boolean autoPush;
+    private Account autoPushInto;
+    private final GregorianCalendar creation;
+    
+    /**
+     * Simple constructor. Gets an ID and a Name
+     * @param id
+     * @param name
+     */
+    public Account(Integer id, String name) {
+        
+        this.id = id;
+        this.name = name;
+        this.balance = 0.0;
+        this.movement = new ArrayList<>();
+        this.autoPull = false;
+        this.autoPush = false;
+        this.creation = new GregorianCalendar();
+        
+    }
+    
+    public Integer getId() {
+        return this.id;
+    }
+    
+    public boolean addTransaction(Transaction t) {
+        return this.movement.add(t);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+
+    public Boolean isAutoPull() {
+        return autoPull;
+    }
+
+    public void setAutoPull(Boolean autoPull) {
+        this.autoPull = autoPull;
+    }
+
+    public Account getAutoPullFrom() {
+        return autoPullFrom;
+    }
+
+    public void setAutoPullFrom(Account autoPullFrom) {
+        this.autoPullFrom = autoPullFrom;
+    }
+
+    public Boolean isAutoPush() {
+        return autoPush;
+    }
+
+    public void setAutoPush(Boolean autoPush) {
+        this.autoPush = autoPush;
+    }
+
+    public Account getAutoPushInto() {
+        return autoPushInto;
+    }
+
+    public void setAutoPushInto(Account autoPushInto) {
+        this.autoPushInto = autoPushInto;
+    }
+    
+}
